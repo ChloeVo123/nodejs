c0144630121b132eaa936555f6ba5800e5e1fa81
HangVo123 <48342625+HangVo123@users.noreply.github.com>
2021-02-22 12:21:53 +1100

    chalk library

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,8 +1,12 @@
-const validator = require('validator')
+//const validator = require('validator')
+
+const chalk = require('chalk')
 
 const getNotes = require('./notes.js')
 const msg = getNotes()
 
 console.log(msg)
 
-console.log(validator.isEmail('andnew@example.com'))
+//console.log(validator.isEmail('andnew@example.com'))
+
+console.log(chalk.blue.bold.italic('hello chalk'))
